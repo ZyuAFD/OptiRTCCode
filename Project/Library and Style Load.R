58eb1396118f78d8edd5b4269752f6f3bb874ac9
Ziwen Yu <ziwenyu83@gmail.com>
2017-07-26 12:17:59 -0400

    Update Library and Style Load.R

--- Project/Library and Style Load.R
+++ Project/Library and Style Load.R
@@ -9,25 +9,21 @@ pacman::p_load(data.table,
                 scales,
                 zoo,
                 Coalesce,
-                knitr)
+                knitr,
+              RcppRoll)
 
 
 #  Plot Theme ------------------
 Plot_theme=theme_bw()+
-  theme(axis.text=element_text(size=14, 
-                               color="grey12", 
-                               face="bold"),
-        axis.title=element_text(size=18,
-                                face="bold"),
+  theme(axis.text=element_text(size=12, 
+                               color="grey12"),
+        axis.title=element_text(size=14),
         plot.title=element_text(size=18,
                                 face="bold"),
         legend.position="bottom",
         legend.title=element_text(size=14),
         legend.text=element_text(size=14, 
-                                 color="grey12", 
-                                 face="bold"),
-        strip.text.y = element_text(size = 12,
-                                    face='bold'),
-        strip.text.x = element_text(size = 12,
-                                    face='bold'),
+                                 color="grey12"),
+        strip.text.y = element_text(size = 12),
+        strip.text.x = element_text(size = 12),
         axis.text.x = element_text(angle = 45, hjust = 1))
